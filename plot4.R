b317af634c66961740333673d81854320ea812ff
Luis Venegas <luisvenegas@ufm.edu>
2020-04-11 19:23:12 -0600

    Adds plot 4

---
+++ plot4.R
@@ -0,0 +1,20 @@
+library(lubridate)
+household = read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = F)
+household$Date_Time = paste(household$Date, household$Time)
+household$Date = dmy(household$Date)
+household$Date_Time = dmy_hms(household$Date_Time)
+household$Global_active_power = as.numeric(household$Global_active_power)
+household = household[household$Date == "2007-02-01" | household$Date == "2007-02-02",]
+
+quartz("plot4.png")
+par(mfrow = c(2,2))
+plot(household$Date_Time, household$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(household$Date_Time, household$Voltage, type = "l", xlab = "", ylab = "Voltage")
+plot(household$Date_Time, household$Sub_metering_1, type = "l", xlab= "", ylab= "Energy sub metering")
+lines(household$Date_Time, household$Sub_metering_2, col= "red")
+lines(household$Date_Time, household$Sub_metering_3, col= "blue")
+legend("topright", legend = c("Sub Metering 1", "Sub Metering 2", "Sub Metering 3"), 
+       col = c("black", "red" , "blue"), pch = "_")
+plot(household$Date_Time, household$Global_reactive_power, type = "l", xlab = "", ylab = "Global Reactive Power")
+quartz.save("plot4.png", type = "png")
+dev.off()
